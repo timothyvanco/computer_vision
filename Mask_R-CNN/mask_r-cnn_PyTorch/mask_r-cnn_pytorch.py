5ad8f733007ea46e8384020f06c6b57186b6cda1
timothyvanco <timco.vanco@gmail.com>
2020-07-06 15:38:17 +0200

    third commit

--- Mask_R-CNN/mask_r-cnn_PyTorch/mask_r-cnn_pytorch.py
+++ Mask_R-CNN/mask_r-cnn_PyTorch/mask_r-cnn_pytorch.py
@@ -3,3 +3,154 @@ import numpy as np
 import torch
 from PIL import Image
 
+class PennFudanDataset(object):
+    def __init__(self, root, transforms):
+        self.root = root
+        self.transforms = transforms
+        self.imgs = list(sorted(os.listdir(os.path.join(root, "PNGImages"))))
+        self.masks = list(sorted(os.listdir(os.path.join(root, "PedMasks"))))
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.root, "PNGImages", self.imgs[idx])
+        mask_path = os.path.join(self.root, "PedMasks", self.masks[idx])
+        img = Image.open(img_path).convert("RGB")
+        mask = Image.open(mask_path)
+        mask = np.array(mask)  # convert the PIL Image into a numpy array
+        obj_ids = np.unique(mask)
+        obj_ids = obj_ids[1:]  # first id is background -> remove it
+
+        # split color-encoded mask into a set of binary masks
+        masks = mask == obj_ids[:, None, None]
+
+        # get bounding box coordinates for each mask
+        num_objs = len(obj_ids)
+        boxes = []
+        for i in range(num_objs):
+            pos = np.where(masks[i])
+            xmin = np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        # convert everything into Torch Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+
+        # there is only one class
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["masks"] = masks
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+
+# replace classifier with a new one, that has num_classes which is user-defined
+num_classes = 2  # 1 class (person) + background
+# get number of input features for the classifier
+in_features = model.roi_heads.box_predictor.cls_score.in_features
+# replace pre-trained head with new one
+model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+
+# load pre-trained model for classification and return only features
+backbone = torchvision.models.mobilenet_v2(pretrained=True).features
+# FasterRCNN needs to know num of output channel in a backbone. For mobilenet_v2
+# it is 1280 so we need to add it here
+backbone.out_channels = 1280
+
+# let's make the RPN generate 5 x 3 anchors per spatial location,
+# with 5 different sizes and 3 different aspect ratios.
+# We have a Tuple[Tuple[int]] because each feature map could potentially
+# have different sizes and aspect ratios
+anchor_generator = AnchorGenerator(sizes=((32, 64, 128, 256, 512),),
+                                   aspect_ratios=((0.5, 1.0, 2.0),))
+
+# let's define what are the feature maps that we will use to perform
+# the region of interest cropping, as well as the size of the crop
+# after rescaling. If your backbone returns a Tensor, featmap_names
+# is expected to be [0]. More generally, the backbone should return an
+# OrderedDict[Tensor], and in featmap_names you can choose which
+# feature maps to use.
+roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=[0],
+                                                output_size=7,
+                                                sampling_ratio=2)
+
+# put pieces together inside a FasterRCNN model
+model = FasterRCNN(backbone,
+                   num_classes=2,
+                   rpn_anchor_generator=anchor_generator,
+                   box_roi_pool=roi_pooler)
+
+
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+
+def get_model_instance_segmentation(num_classes):
+    # model pretrained on COCO dataset
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+
+    # get number of inpput features for classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace pre trained head with new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    # get number of input features for mask classifier
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = 256
+
+    # replace mask predictor with new one
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
+                                                       hidden_layer,
+                                                       num_classes)
+    return model
+
+import transforms as T
+
+def get_transforms(train):
+    transforms = []
+    transforms.append(T.ToTensor())
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    return T.Compose(transforms)
+
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+dataset = PennFudanDataset('PennFudanPed', get_transforms(train=True))
+data_loader = torch.utils.data.DataLoader(dataset,
+                                          batch_size=2,
+                                          shuffle=True,
+                                          num_workers=4,
+                                          collate_fn=utils.collate_fn)
+images, targets = next(iter(data_loader))
+images = list(image for image in images)
+targets = [{k: v for k, v in t.items()} for t in targets]
+output = model(images, targets)  # Return losses and detections
+model.eval()
+x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
+predictions = model(x)  # returns predictions
+
